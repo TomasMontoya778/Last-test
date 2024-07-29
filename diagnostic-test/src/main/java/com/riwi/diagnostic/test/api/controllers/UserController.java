62a504a009def879459dd5feec8af09dbc629dd5
Tomás Montoya <tomas1213monto@gmail.com>
2024-07-29 14:07:59 -0500

    Documentation user

--- diagnostic-test/src/main/java/com/riwi/diagnostic/test/api/controllers/UserController.java
+++ diagnostic-test/src/main/java/com/riwi/diagnostic/test/api/controllers/UserController.java
@@ -11,10 +11,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.riwi.diagnostic.test.api.dto.errors.BaseErrorResponse;
 import com.riwi.diagnostic.test.api.dto.requests.UserRequest;
 import com.riwi.diagnostic.test.api.dto.response.UserResponse;
 import com.riwi.diagnostic.test.infrastructure.abstracts_services.IUserService;
-    
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 
@@ -25,11 +31,81 @@ import lombok.AllArgsConstructor;
 public class UserController {
     @Autowired
     private final IUserService userService;
-
+    
+    @Operation(
+        summary = "Get all Users",
+        description = "Retrieve a paginated list of all users",
+        parameters = {
+            @Parameter(
+                name = "page", 
+                description = "Page number", 
+                schema = @Schema(type = "integer", defaultValue = "1")),
+            @Parameter(
+                name = "size", 
+                description = "Page size", 
+                schema = @Schema(type = "integer", defaultValue = "10")),
+            @Parameter(
+                name = "username", 
+                description = "Filter by username", 
+                schema = @Schema(type = "string")),
+            @Parameter( 
+                name = "email", 
+                description = "Filter by email", 
+                schema = @Schema(type = "string")),
+        },
+         responses = {
+            @ApiResponse(
+                responseCode = "200", description = "SUCCESSFUL", 
+                content = @Content(schema = @Schema(implementation = Page.class))),
+            @ApiResponse(
+                responseCode = "401", description = "NOT AUTHORIZED", 
+                content = @Content(schema = @Schema(implementation = BaseErrorResponse.class))),
+            @ApiResponse(
+                responseCode = "403", description = "FORBIDDEN ACCESS", 
+                content = @Content(schema = @Schema(implementation = BaseErrorResponse.class))),
+            @ApiResponse(
+                responseCode = "500", description = "INTERNAL SERVER ERROR", 
+                content = @Content(schema = @Schema(implementation = BaseErrorResponse.class)))
+        }
+    )
     @PostMapping("/create")
     public ResponseEntity<UserResponse> create(@Validated @RequestBody UserRequest request){
         return ResponseEntity.ok(userService.create(request));
     }
+    @Operation(
+        summary = "Get all Users",
+        description = "Retrieve a paginated list of all users",
+        parameters = {
+            @Parameter(
+                name = "page", 
+                description = "Page number", 
+                schema = @Schema(type = "integer", defaultValue = "1")),
+            @Parameter(
+                name = "size", 
+                description = "Page size", 
+                schema = @Schema(type = "integer", defaultValue = "10")),
+            @Parameter(
+                name = "username", 
+                description = "Filter by username", 
+                schema = @Schema(type = "string")),
+        },
+         responses = {
+            @ApiResponse(
+                responseCode = "200", description = "SUCCESSFUL", 
+                content = @Content(schema = @Schema(implementation = Page.class))),
+            @ApiResponse(
+                responseCode = "400", description = "BAD REQUEST", 
+                content = @Content(schema = @Schema(implementation = BaseErrorResponse.class))),
+            @ApiResponse(
+                responseCode = "401", description = "NOT AUTHORIZED", 
+                content = @Content(schema = @Schema(implementation = BaseErrorResponse.class))),
+            @ApiResponse(
+                responseCode = "403", description = "FORBIDDEN ACCESS", 
+                content = @Content(schema = @Schema(implementation = BaseErrorResponse.class))),
+            @ApiResponse(
+                responseCode = "500", description = "INTERNAL SERVER ERROR", 
+                content = @Content(schema = @Schema(implementation = BaseErrorResponse.class)))
+        })
     @GetMapping("/all")
     public ResponseEntity<Page<UserResponse>> getAll(@Validated Pageable pageable){
         return ResponseEntity.ok(userService.getAll(pageable));
